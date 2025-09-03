dad3dc7463a2abd30b09dc7b4646bfc936ec9b66
Yash Khandelwal <yashsk23.12@gmail.com>
2025-09-03 14:46:58 +0530

    setup first spring project

---
+++ src/test/java/com/adslide/auction/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adslide.auction.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
